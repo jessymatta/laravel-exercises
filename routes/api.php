a324174faa0b879313604e442b0349439547a3ec
jessymatta <jessy.w.matta@gmail.com>
2022-09-30 23:02:33 +0300

    Finalized the prefixEvaluation() function in api4

--- routes/api.php
+++ routes/api.php
@@ -29,4 +29,4 @@ Route::get("api2/{input_int}", [laravelEx::class,'placeValue']);
 Route::get("api3/{input_string}", [laravelEx::class,'convertToBinary']);
 
 //Route for api4 : that evaluate prefix notations
-Route::get("api4", [laravelEx::class,'prefixEvalution']);
+Route::get("api4/{input_str}", [laravelEx::class,'prefixEvalution']);
